e7bc13901876054cdd10da32a8c4d156807dc4f0
Lucas Tierney <lucast1533@gmail.com>
2018-07-17 20:24:42 -0500

    get rid of jdk_switcher call

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -3,6 +3,6 @@ set -ex
 sh /etc/init.d/xvfb start
 
 if [[ ! -z $TOXENV ]]; then
-  . jdk_switcher.sh && jdk_switcher use oraclejdk8
+  . jdk_switcher use oraclejdk8
   ./go py_prep_for_install_release
 fi
