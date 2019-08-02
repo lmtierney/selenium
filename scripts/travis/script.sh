78257a53d37d7e8986f6509574560abbd3d18077
Lucas Tierney <lucast1533@gmail.com>
2019-08-01 20:36:53 -0500

    testing travis change

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -4,13 +4,16 @@ if [[ $TOXENV == *"remote"* ]]; then
   ./go selenium-server-standalone
 fi
 
-echo $TRAVIS_PULL_REQUEST
-echo "hi"
-echo "$TRAVIS_PULL_REQUEST"
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+  echo "not PR"
+else
+  echo $TRAVIS_PULL_REQUEST
+fi
+
 
 # Tests will be run only when each folder, such as /py, is modified
 if [[ ! -z $TOXENV ]]; then
-  if [[ $TRAVIS_PULL_REQUEST == "false" ]] || [[ git diff --name-only HEAD~1| grep '^py/' >/dev/null ]]; then
+  if [[ $TRAVIS_PULL_REQUEST == "false" ]] || git diff --name-only HEAD~1| grep '^py/' >/dev/null; then
     tox -c py/tox.ini
   fi
 fi
@@ -18,7 +21,7 @@ fi
 # Ordering matters here. We want rake tasks to run first
 if [[ ! -z $TASK ]]; then
   if [[ $TASK == //rb:* ]]; then
-     if [[ $TRAVIS_PULL_REQUEST == "false" ]] || [[ git diff --name-only HEAD~1| grep '^rb/' >/dev/null ]]; then
+     if [[ $TRAVIS_PULL_REQUEST == "false" ]] || git diff --name-only HEAD~1| grep '^rb/' >/dev/null; then
        ./go $TASK
      fi
   else
@@ -28,11 +31,11 @@ fi
 
 if [[ ! -z "$BUCK" ]]; then
   if [[ $BUCK == test\ //javascript/* ]]; then
-     if [[ $TRAVIS_PULL_REQUEST == "false" ]] || [[ git diff --name-only HEAD~1| grep '^javascript/' >/dev/null ]]; then
+     if [[ $TRAVIS_PULL_REQUEST == "false" ]] || git diff --name-only HEAD~1| grep '^javascript/' >/dev/null; then
        ./buckw $BUCK
      fi
   elif [[ $BUCK == test\ * ]]; then
-     if [[ $TRAVIS_PULL_REQUEST == "false" ]] || [[ git diff --name-only HEAD~1| grep '^java/' >/dev/null ]]; then
+     if [[ $TRAVIS_PULL_REQUEST == "false" ]] || git diff --name-only HEAD~1| grep '^java/' >/dev/null; then
        ./buckw $BUCK
      fi
   else
@@ -41,7 +44,7 @@ if [[ ! -z "$BUCK" ]]; then
 fi
 
 if [[ ! -z "$NPM" ]]; then
-  if [[ $TRAVIS_PULL_REQUEST == "false" ]] || [[ git diff --name-only HEAD~1| grep '^javascript/' >/dev/null ]]; then
+  if [[ $TRAVIS_PULL_REQUEST == "false" ]] || git diff --name-only HEAD~1| grep '^javascript/' >/dev/null; then
     ./go node:atoms
     cd javascript/node/selenium-webdriver; npm install; npm run $NPM
   fi
